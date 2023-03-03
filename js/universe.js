32199c5341d6ebb4974e0398b123fc8345183c92
Soikat07 <sahasoikat94@gmail.com>
2023-03-03 08:23:09 +0600

    modal add

--- js/universe.js
+++ js/universe.js
@@ -8,8 +8,8 @@ const displayCardData=allCardData=>{
      // console.log(allCardData);
      const cardContainer=document.getElementById('card-container');
      allCardData.slice(0,6).forEach(singleCardData => {
-          console.log(singleCardData);
-          const{image, features, name, published_in}=singleCardData;
+          // console.log(singleCardData);
+          const{image, features, name, published_in,id}=singleCardData;
           cardContainer.innerHTML+=`
           <div class="col">
           <div class="card h-100">
@@ -29,7 +29,7 @@ const displayCardData=allCardData=>{
            <i class="fa-solid fa-calendar-days mb-3"></i>
            <p>${published_in}</p>
            </div>   
-           <i class="fa-solid fa-arrow-right text-danger pe-4"></i>
+           <i onclick="loadSingleModalData('${id}')" class="fa-solid fa-arrow-right text-danger pe-4 "data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
            </div>
            </div>
           </div>
@@ -37,4 +37,33 @@ const displayCardData=allCardData=>{
           `
      });
 }
+// Modal section
+     const loadSingleModalData=(cardId)=>{
+          const url=`https://openapi.programming-hero.com/api/ai/tool/${cardId}`
+          fetch(url)
+          .then(res => res.json())
+          .then(data =>showSingleModalData(data.data))
+     }
+     const showSingleModalData=data=>{
+          console.log(data);
+          document.getElementById('modal-container').innerHTML=`
+          <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">${data.description}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col text-center">
+          <div class="card h-100">
+            <img src="${data.image_link[0]}" class="card-img-top p-2 rounded-5" alt="...">
+            <div class="card-body">
+               <h5 class="card-title">${data.input_output_examples[0].input}</h5>
+              <p>${data.input_output_examples[0].output}</p>
+            </div>
+          </div>
+        </div>
+          `
+     }
 loadCardAllData()
